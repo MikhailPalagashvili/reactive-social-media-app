c3c648aa537c7d6d0066050231a636fabfd4429e
Mikhail Palagashvili <48160170+MikhailPalagashvili@users.noreply.github.com>
2021-04-16 12:57:59 -0400

    Initial commit

---
+++ src/main/java/com/grantcs/reactivesocialmediaapp/ReactiveSocialMediaAppApplication.java
@@ -0,0 +1,13 @@
+package com.grantcs.reactivesocialmediaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSocialMediaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSocialMediaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grantcs/reactivesocialmediaapp/ReactiveSocialMediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grantcs.reactivesocialmediaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSocialMediaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
